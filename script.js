4cafc8d3ce05eb971991a76d24c8df5bca03c6ff
ryo-a <ryoa.dev@gmail.com>
2020-03-18 02:34:08 +0900

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let currentMusic = "";
+const displayNowPlaying = function(){
+    let np = document.getElementsByClassName("now-playing")
+    let nowPlayingFromDOM =  np[0].getAttribute("aria-label");
+    if (currentMusic != nowPlayingFromDOM){
+      console.log(nowPlayingFromDOM);
+      currentMusic = nowPlayingFromDOM;
+    }
+} 
+setInterval(displayNowPlaying, 1000);
